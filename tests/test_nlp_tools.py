3f92ea958d7c47f0788c6b0b71b847e418d68d35
Ali Abbas <ali.abbas@aquantix.ca>
2021-02-10 17:28:51 -0500

    Minor Changes

--- tests/test_nlp_tools.py
+++ tests/test_nlp_tools.py
@@ -1,8 +1,8 @@
 import unittest
 from murphy.data_loader import DataLoader
 from tests import CommonTestSetup
-import dask.dataframe as dd
 from murphy.nlp_tools import NLPTools
+from itertools import product
 
 
 class NLPTestCase(unittest.TestCase):
@@ -11,6 +11,7 @@ class NLPTestCase(unittest.TestCase):
         self.data_loader = DataLoader(
             file_find_expression = self.path_prefix / 'data/test_data/test_sample_files.json.bz2'
         )
+
         self._test_string = "From: (where's my thing) Subject: WHAT car is this!? Nntp-Posting-Host: rac3.wam.umd.edu " \
                             "Organization: University of Maryland, College Park Lines: 15 I was wondering if anyone " \
                             "out there could enlighten me on this car I saw the other day. It was a 2-door sports " \
@@ -37,32 +38,45 @@ class NLPTestCase(unittest.TestCase):
                                          "model name, engine specs, years production, car made, history, " \
                                          "whatever info funky looking car, please e-mail. Thanks, - IL ---- brought " \
                                          "neighborhood Lerxst ---- "
+
         self._lemmatized_string = "from : ( where be my thing ) subject : what car be this ! ? Nntp - posting - Host : " \
-                                 "rac3.wam.umd.edu Organization : University of Maryland , College Park line : 15 I " \
-                                 "be wonder if anyone out there could enlighten I on this car I see the other day . " \
-                                 "it be a 2 - door sport car , look to be from the late 60s/ early 70 . it be call a " \
-                                 "Bricklin . the door be really small . in addition , the front bumper be separate " \
-                                 "from the rest of the body . this be all I know . if anyone can tellme a model name " \
-                                 ", engine spec , year of production , where this car be make , history , or whatever " \
-                                 "info you have on this funky look car , please e - mail . thank , - IL ---- bring to " \
-                                 "you by your neighborhood Lerxst ----"
+                                  "rac3.wam.umd.edu Organization : University of Maryland , College Park line : 15 I " \
+                                  "be wonder if anyone out there could enlighten I on this car I see the other day . " \
+                                  "it be a 2 - door sport car , look to be from the late 60s/ early 70 . it be call a " \
+                                  "Bricklin . the door be really small . in addition , the front bumper be separate " \
+                                  "from the rest of the body . this be all I know . if anyone can tellme a model name " \
+                                  ", engine spec , year of production , where this car be make , history , or whatever " \
+                                  "info you have on this funky look car , please e - mail . thank , - IL ---- bring to " \
+                                  "you by your neighborhood Lerxst ----"
 
         self.nlp_tool = NLPTools()
 
-    def test_tokenize(self):
+    def test_tokenize_func(self):
         self.assertEqual(
             self.nlp_tool._tokenize(self._test_string),
             self._tokenized_test_string
         )
 
-    def test_remove_stopwords(self):
+    def test_remove_stopwords_func(self):
         self.assertEqual(
-            self.nlp_tool.remove_stopwords(self._test_string),
+            self.nlp_tool._remove_stopwords(self._test_string),
             self._no_stop_word_test_string
         )
 
-    def test_lemmatize(self):
+    def test_lemmatize_func(self):
         self.assertEqual(
-            self.nlp_tool.lemmatize(self._test_string),
+            self.nlp_tool._lemmatize(self._test_string),
             self._lemmatized_string
         )
+
+    def test_run_nlp_tools(self):
+        bool_flag = [True, False]
+        for tokenize_flag, filter_stopwords_flag, lemmatize_flag in product(bool_flag, bool_flag, bool_flag):
+           nlp_tool_obj = NLPTools(
+               tokenize = tokenize_flag,
+               filter_stopwords = filter_stopwords_flag,
+               lemmatize = lemmatize_flag
+           )
+
+           if tokenize_flag:
+                pass
