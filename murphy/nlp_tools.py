8f93b81a9a957190740beb76089ec5f9bb0bb3de
Ali Abbas <ali.abbas@aquantix.ca>
2021-02-10 17:28:02 -0500

    Added better functionality

--- murphy/nlp_tools.py
+++ murphy/nlp_tools.py
@@ -1,6 +1,9 @@
 from dask.dataframe import DataFrame as dask_dataframe
+import pandas as pd
 import en_core_web_sm
 import nltk
+
+from typing import Union
 
 try:
     nltk.data.find('tokenizers/punkt')
@@ -33,35 +36,47 @@ class NLPTools:
         tokens_lst = list(filter(lambda word: word.isalnum(), tokens))
         return ' '.join(tokens_lst)
 
-    def remove_stopwords(self, string: str):
+    def _remove_stopwords(self, string: str):
         filtered = filter(lambda word: word not in self.stopwords, string.split(' '))
         return ' '.join(filtered)
 
     @staticmethod
-    def lemmatize(string):
+    def _lemmatize(string):
         doc = NLPTools.nlp(string)
         lemmatized = [token.lemma_ for token in doc]
 
         return ' '.join(lemmatized)
 
-    def run_tools(self, twitter_dataframe: dask_dataframe):
+    def tokenize_tweets(self, tweet_dataframe: Union[dask_dataframe, pd.DataFrame]) -> Union[dask_dataframe, pd.DataFrame]:
+        tweet_dataframe['text'] = tweet_dataframe['text'].apply(
+            lambda text: self._tokenize(text),
+            meta = str
+        )
+        return tweet_dataframe
+
+    def filter_stopwords(self, tweet_dataframe: dask_dataframe) -> Union[dask_dataframe, pd.DataFrame]:
+        tweet_dataframe['text'] = tweet_dataframe.apply(
+            lambda x: self._remove_stopwords(x['text']),
+            axis = 1, meta = str
+        )
+        return tweet_dataframe
+
+    def run_tools(self, tweet_dataframe: dask_dataframe) -> Union[dask_dataframe, pd.DataFrame]:
         if self.tokenize_flag:
-            twitter_dataframe['text'] = twitter_dataframe.apply(
-                lambda x: self._tokenize(x['text']),
-                axis = 1, meta = str
-            )
+            tweet_dataframe = self.tokenize_tweets(tweet_dataframe)
 
         if self.filter_stopwords_flag:
-            twitter_dataframe['text'] = twitter_dataframe.apply(
-                lambda x: self.remove_stopwords(x['text']),
+            tweet_dataframe['text'] = tweet_dataframe.apply(
+                lambda x: self._remove_stopwords(x['text']),
                 axis = 1, meta = str
             )
+            # tweet_dataframe = self.filter_stopwords(tweet_dataframe)
 
         if self.lemmatize_flag:
-            twitter_dataframe['text'] = twitter_dataframe.apply(
-                lambda x: self.lemmatize(x['text']),
+            tweet_dataframe['text'] = tweet_dataframe.apply(
+                lambda x: self._lemmatize(x['text']),
                 axis = 1,
                 meta = str
             )
 
-        return twitter_dataframe
+        return tweet_dataframe
